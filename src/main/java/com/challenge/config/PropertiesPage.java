8691d73eb100c02802ea838742ea8e81921c1b34
Ramses <ramsesortiz.ar@gmail.com>
2022-10-10 01:10:39 -0300

    Readme added

--- src/main/java/com/challenge/config/PropertiesPage.java
+++ src/main/java/com/challenge/config/PropertiesPage.java
@@ -4,8 +4,9 @@ public class PropertiesPage {
 
     public static final String URL = "https://www.demoblaze.com/index.html";
 
-    public static String browser = "chrome";
+    public static String browser = "chrome"; //Select chrome or firefox
 
+    //Data random for execution
     public static String username = "Test " + (Math.random()*1000000 + 1);
 
     public static String password = "Test " + (Math.random()*1000000 + 1);
